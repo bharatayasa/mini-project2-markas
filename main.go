3132f2afd01bab339882732a0326f9a115f1cfcc
Bharata <bharata@Bharatas-MacBook-Air.local>
2024-03-16 17:32:56 +0800

    done

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -25,48 +24,58 @@ type Book struct {
 var booksDirectory = "books"
 var pdfDirectory = "pdf"
 
-func inputBook() {
+func TambahBuku() {
 	var newBook Book
+	for {
+		fmt.Print("masukkan kode buku: ")
+		fmt.Scanln(&newBook.Kode)
 
-	fmt.Println("\nMasukkan detail buku:")
-	fmt.Print("Kode: ")
-	fmt.Scanln(&newBook.Kode)
+		if _, err := os.Stat(filepath.Join(booksDirectory, "book-"+newBook.Kode+".json")); !os.IsNotExist(err) {
+			fmt.Println("Kode buku sudah digunakan.")
+			return
+		}
 
-	// Check apakah kode buku sudah ada
-	if _, err := os.Stat(filepath.Join(booksDirectory, "book-"+newBook.Kode+".json")); !os.IsNotExist(err) {
-		fmt.Println("Kode buku sudah digunakan.")
-		return
+		fmt.Print("masukkan judul buku: ")
+		fmt.Scanln(&newBook.Judul)
+		fmt.Print("masukkan pengarang buku: ")
+		fmt.Scanln(&newBook.Pengarang)
+		fmt.Print("masukkan penerbit buku: ")
+		fmt.Scanln(&newBook.Penerbit)
+		fmt.Print("masukkan jumlah halaman buku: ")
+		fmt.Scanln(&newBook.JumlahHalaman)
+		fmt.Print("masukkan tahun terbit buku: ")
+		fmt.Scanln(&newBook.TahunTerbit)
+
+		bookData, err := json.MarshalIndent(newBook, "", "    ")
+		if err != nil {
+			fmt.Println("Error encoding book data:", err)
+			return
+		}
+
+		err = os.WriteFile(filepath.Join(booksDirectory, "book-"+newBook.Kode+".json"), bookData, 0644)
+		if err != nil {
+			fmt.Println("Error saving book data:", err)
+			return
+		}
+
+		var pilihanMenuPesanan = 0
+		fmt.Println("Ketik 1 untuk tambah pesanan, ketik 0 untuk keluar")
+		_, err = fmt.Scanln(&pilihanMenuPesanan)
+		if err != nil {
+			fmt.Println("Terjadi Error:", err)
+			return
+		}
+
+		if pilihanMenuPesanan == 0 {
+			break
+		}
+
+		fmt.Println("Buku berhasil disimpan.")
 	}
-
-	fmt.Print("Judul: ")
-	fmt.Scanln(&newBook.Judul)
-	fmt.Print("Pengarang: ")
-	fmt.Scanln(&newBook.Pengarang)
-	fmt.Print("Penerbit: ")
-	fmt.Scanln(&newBook.Penerbit)
-	fmt.Print("Jumlah Halaman: ")
-	fmt.Scanln(&newBook.JumlahHalaman)
-	fmt.Print("Tahun Terbit: ")
-	fmt.Scanln(&newBook.TahunTerbit)
-
-	// Simpan ke file JSON
-	bookData, err := json.MarshalIndent(newBook, "", "  ")
-	if err != nil {
-		fmt.Println("Error encoding book data:", err)
-		return
-	}
-
-	err = ioutil.WriteFile(filepath.Join(booksDirectory, "book-"+newBook.Kode+".json"), bookData, 0644)
-	if err != nil {
-		fmt.Println("Error saving book data:", err)
-		return
-	}
-
-	fmt.Println("Buku berhasil disimpan.")
 }
 
-func listBooks() {
-	files, err := ioutil.ReadDir(booksDirectory)
+func TampilBuku() {
+	files, err := os.ReadDir(booksDirectory)
 	if err != nil {
 		fmt.Println("Error reading books directory:", err)
 		return
@@ -75,7 +84,7 @@ func listBooks() {
 	fmt.Println("\nDaftar Buku:")
 	for _, file := range files {
 		if strings.HasPrefix(file.Name(), "book-") && strings.HasSuffix(file.Name(), ".json") {
-			bookData, err := ioutil.ReadFile(filepath.Join(booksDirectory, file.Name()))
+			bookData, err := os.ReadFile(filepath.Join(booksDirectory, file.Name()))
 			if err != nil {
 				fmt.Printf("Error reading book %s: %v\n", file.Name(), err)
 				continue
@@ -93,7 +102,7 @@ func listBooks() {
 	}
 }
 
-func editBook() {
+func EditBook() {
 	var kode string
 	fmt.Print("Masukkan kode buku yang ingin diubah: ")
 	fmt.Scanln(&kode)
@@ -105,37 +114,50 @@ func editBook() {
 		return
 	}
 
-	var newBook Book
+	var editedBook Book
 
-	fmt.Println("\nMasukkan detail buku baru:")
-	fmt.Print("Judul: ")
-	fmt.Scanln(&newBook.Judul)
-	fmt.Print("Pengarang: ")
-	fmt.Scanln(&newBook.Pengarang)
-	fmt.Print("Penerbit: ")
-	fmt.Scanln(&newBook.Penerbit)
-	fmt.Print("Jumlah Halaman: ")
-	fmt.Scanln(&newBook.JumlahHalaman)
-	fmt.Print("Tahun Terbit: ")
-	fmt.Scanln(&newBook.TahunTerbit)
-
-	// Simpan ke file JSON
-	bookData, err := json.MarshalIndent(newBook, "", "  ")
+	bookData, err := os.ReadFile(filePath)
 	if err != nil {
-		fmt.Println("Error encoding book data:", err)
+		fmt.Println("Error reading book data:", err)
 		return
 	}
 
-	err = ioutil.WriteFile(filePath, bookData, 0644)
+	err = json.Unmarshal(bookData, &editedBook)
 	if err != nil {
-		fmt.Println("Error saving book data:", err)
+		fmt.Println("Error decoding book data:", err)
+		return
+	}
+
+	editedBook.Kode = kode
+
+	fmt.Println("Masukkan informasi buku yang baru:")
+	fmt.Print("masukkan judul buku baru: ")
+	fmt.Scanln(&editedBook.Judul)
+	fmt.Print("masukkan pengarang baru: ")
+	fmt.Scanln(&editedBook.Pengarang)
+	fmt.Print("masukkan penerbit baru: ")
+	fmt.Scanln(&editedBook.Penerbit)
+	fmt.Print("masukkan jumlah halaman baru: ")
+	fmt.Scanln(&editedBook.JumlahHalaman)
+	fmt.Print("masukkan tahun terbit baru: ")
+	fmt.Scanln(&editedBook.TahunTerbit)
+
+	editedBookData, err := json.MarshalIndent(editedBook, "", "    ")
+	if err != nil {
+		fmt.Println("Error encoding edited book data:", err)
+		return
+	}
+
+	err = os.WriteFile(filePath, editedBookData, 0644)
+	if err != nil {
+		fmt.Println("Error saving edited book data:", err)
 		return
 	}
 
 	fmt.Println("Buku berhasil diubah.")
 }
 
-func deleteBook() {
+func HapusBuku() {
 	var kode string
 	fmt.Print("Masukkan kode buku yang ingin dihapus: ")
 	fmt.Scanln(&kode)
@@ -156,28 +178,6 @@ func deleteBook() {
 	fmt.Println("Buku berhasil dihapus.")
 }
 
-func printMenu() {
-	fmt.Println("\nPrint Menu:")
-	fmt.Println("1. Print Satu Buku")
-	fmt.Println("2. Print Semua Buku")
-	fmt.Println("3. Kembali ke Menu Utama")
-
-	var choice int
-	fmt.Print("Pilih menu: ")
-	fmt.Scanln(&choice)
-
-	switch choice {
-	case 1:
-		printOneBook()
-	case 2:
-		printAllBooks()
-	case 3:
-		return
-	default:
-		fmt.Println("Pilihan tidak valid.")
-	}
-}
-
 func printOneBook() {
 	var kode string
 	fmt.Print("Masukkan kode buku yang ingin di-print: ")
@@ -190,7 +190,7 @@ func printOneBook() {
 		return
 	}
 
-	bookData, err := ioutil.ReadFile(filePath)
+	bookData, err := os.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("Error reading book data:", err)
 		return
@@ -207,7 +207,7 @@ func printOneBook() {
 }
 
 func printAllBooks() {
-	files, err := ioutil.ReadDir(booksDirectory)
+	files, err := os.ReadDir(booksDirectory)
 	if err != nil {
 		fmt.Println("Error reading books directory:", err)
 		return
@@ -215,7 +215,7 @@ func printAllBooks() {
 
 	for _, file := range files {
 		if strings.HasPrefix(file.Name(), "book-") && strings.HasSuffix(file.Name(), ".json") {
-			bookData, err := ioutil.ReadFile(filepath.Join(booksDirectory, file.Name()))
+			bookData, err := os.ReadFile(filepath.Join(booksDirectory, file.Name()))
 			if err != nil {
 				fmt.Printf("Error reading book %s: %v\n", file.Name(), err)
 				continue
@@ -262,15 +262,35 @@ func printBookToPDF(book Book) {
 	fmt.Println("Buku", book.Judul, "telah di-print ke file", fileName)
 }
 
+func PrintMenu() {
+	fmt.Println("\nPrint Menu:")
+	fmt.Println("1. Print Satu Buku")
+	fmt.Println("2. Print Semua Buku")
+	fmt.Println("3. Kembali ke Menu Utama")
+
+	var choice int
+	fmt.Print("Pilih menu: ")
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case 1:
+		printOneBook()
+	case 2:
+		printAllBooks()
+	case 3:
+		return
+	default:
+		fmt.Println("Pilihan tidak valid.")
+	}
+}
+
 func main() {
-	// Membuat direktori books jika belum ada
 	err := os.MkdirAll(booksDirectory, os.ModePerm)
 	if err != nil {
 		fmt.Println("Error creating books directory:", err)
 		return
 	}
 
-	// Membuat direktori pdf jika belum ada
 	err = os.MkdirAll(pdfDirectory, os.ModePerm)
 	if err != nil {
 		fmt.Println("Error creating pdf directory:", err)
@@ -278,13 +298,16 @@ func main() {
 	}
 
 	for {
-		fmt.Println("\nMain Menu:")
-		fmt.Println("1. Input Buku")
-		fmt.Println("2. List Buku")
+		fmt.Print("---------------------------------------------")
+		fmt.Println("\nSistem manajemen buku")
+		fmt.Println("---------------------------------------------")
+		fmt.Println("1. Tambah Buku")
+		fmt.Println("2. Tampil Buku")
 		fmt.Println("3. Edit Buku")
 		fmt.Println("4. Hapus Buku")
 		fmt.Println("5. Print Buku")
 		fmt.Println("6. Keluar")
+		fmt.Println("---------------------------------------------")
 
 		var choice int
 		fmt.Print("Pilih menu: ")
@@ -292,17 +315,18 @@ func main() {
 
 		switch choice {
 		case 1:
-			inputBook()
+			TambahBuku()
 		case 2:
-			listBooks()
+			TampilBuku()
 		case 3:
-			editBook()
+			EditBook()
 		case 4:
-			deleteBook()
+			HapusBuku()
 		case 5:
-			printMenu()
+			PrintMenu()
 		case 6:
 			fmt.Println("Terima kasih telah menggunakan program ini.")
+			os.Exit(0)
 			return
 		default:
 			fmt.Println("Pilihan tidak valid.")
